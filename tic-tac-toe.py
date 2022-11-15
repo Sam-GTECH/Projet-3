07298fc2b20e64c28b4adf1c201e0d54906b6de7
Sam-GTECH <sburton@gaming.tech>
2022-11-15 11:36:47 +0100

    Still unfinished

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,11 +1,10 @@
 import random
 
-grid = [[0, 0, 0],
+grid = [[1, 0, 1],
         [0, 0, 0],
         [0, 0, 0]]
 
-playerTurn = random.randint(0, 1)==0
-print(playerTurn)
+playerTurn = False#random.randint(0, 1)==0
 
 choice = None
 
@@ -35,8 +34,6 @@ def checkGrid(x:int, y:int=-1)->bool:
     return grid[x][y]==0
 
 def convertToGridIndex(x:int)->tuple:
-    print(x)
-    print(x//3)
     if x<=3:
         x, y = 0, x-1
     elif x<=6:
@@ -45,46 +42,69 @@ def convertToGridIndex(x:int)->tuple:
         x, y = 2, x-7
     return x, y
 
-def checkVictory():
+def checkVictory(nb:int)->bool:
     verif = 0
     # Vertical lines
-    for i in grid:
-        for j in i:
-            if j==1:
+    for x in grid:
+        for y in x:
+            if y==nb:
                 verif=verif+1
+                if verif==3:
+                    return True
             else:
                 verif = 0
                 break
     
-    for i in range(0,2):
-        for j in range(0, 2):
-            if grid[i][j]==1:
-                verif = verif + 1
+    #Horrizental lines
+    for y in range(3):
+        for x in range(3):
+            if grid[x][y]==nb:
+                verif=verif+1
+                if verif==3:
+                    return True
             else:
-                verif = -999
+                verif = 0
+                break
+    
+    #Diagonal lines
+    if (grid[0][0]==nb and grid[1][1]==nb and grid[2][2]==nb) or (grid[2][0]==nb and grid[1][1]==nb and grid[0][2]==nb):
+        return True
+    return False
+
+def ordiChoice():
+    #TODO
+                
     
 
 
 while True:
-    printGrid()
     if playerTurn:
-        while choice==None or not checkGrid(choice):
+        while choice==None:
+            printGrid()
             choice = input("It's your turn! (1-9) ")
             try:
                 choice = int(choice)
                 if choice<1 or choice>9:
                     raise IndexError
+                if not checkGrid(choice):
+                    choice = None
+                    print("The chosen case is already taken.")
             except:
-                raise TypeError(f"The user choice should be an integer")
+                raise TypeError("The user choice should be an integer")
     else:
+        print("It's the ordi's turn!")
         while choice==None or not checkGrid(choice):
-            print("It's the ordi's turn!")
-            choice = random.randint(1, 9)
+            choice = ordiChoice()
     x, y = convertToGridIndex(choice)
 
-    print(x, y)
     grid[x][y] = playerTurn and 1 or 2
     choice = None
     playerTurn = not playerTurn
 
-    checkVictory()
+    if checkVictory(1):
+        print("\n\nThe user won!!")
+        break
+    elif checkVictory(2):
+        print("\n\nThe ordi won!")
+        break
+printGrid()
