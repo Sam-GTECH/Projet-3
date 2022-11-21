47a448abe868aa31acd600d256101e96e93b52f5
mathias <mlipko@gaming.tech>
2022-11-21 16:02:13 +0100

    amazing pseudo code

--- tic-tac-toe [IA].py
+++ tic-tac-toe [IA].py
@@ -90,86 +90,149 @@ def getBoxValue(i):
     x, y = convertToGridIndex(i)
     return grid[x][y]
 
-
+#définir une fonction ordiChoice qui permettra à l'IA de faire des choix
 def ordiChoice():
+    #définition d'un tableau corner possédant les coins de la grille
     corners = [1, 3, 7, 9]
+    #mélange les nombres dans la liste
     random.shuffle(corners)
-
+    #définir ligne à 0
     ligne = 0
+    #définir possibleLastBox à 0
     possibleLastBox = None
-
+    #définir une variable winBox à None
     winBox = None
+    #définir une variable blockBox à None
     blockBox = None
-
-    # First diagonal line
+    
+    #First diagonal line
+    #faire une boucle dans la diagonal du coin supérieur gauche au coin inférieur droit:
     for i in [1, 5, 9]:
+        #convertir l'index i en position x, y
         x, y = convertToGridIndex(i)
+        #Si grille x, y vaut 1
         if grid[x][y]==1:
+            #incrémenter ligne de 1
             ligne=ligne+1
+        #Sinon si grille x, y vaut 2:
         elif grid[x][y]==2:
+            #décrementer ligne de 1
             ligne=ligne-1
+        #Sinon
         else:
+            #Si possibleLastBox ne vaut pas None
             if possibleLastBox!=None:
+                #quitter la boucle
                 break
-            #if grid[x][y]!=2:
+            #mettre possibleLastBox à i
             possibleLastBox = i
+    #Si ligne vaut 2 et possibleLasteBox existe et que la valeur que renvoie getBoxValue(possibleLastBox) vaut 0:
     if ligne == 2 and possibleLastBox and getBoxValue(possibleLastBox)==0:
+        #mettre blockBox à possibleLastBox
         blockBox = possibleLastBox
+        #mettre ligne à 0
         ligne = 0
+        #mettre possibleLastBox à None
         possibleLastBox = None
+    #Sionon si ligne vaut -2 et possibleLasteBox existe et que la valeur que renvoie getBoxValue(possibleLastBox) vaut 0:
     elif ligne == -2 and possibleLastBox and getBoxValue(possibleLastBox)==0:
+        #mettre winBox à possibleLastBox
         winBox = possibleLastBox
+        #mettre ligne à 0
         ligne = 0
+        #mettre possibleLastBox à None
         possibleLastBox = None
+    #Sinon:
     else:
+        #mettre ligne à 0
         ligne = 0
+        #mettre possibleLastBox à 0
         possibleLastBox = None
 
-    # Second diagonal line
+    #Second diagonal line
+    #faire une boucle dans la diagonal du coin supérieur droit au coin inférieur gauche:
     for i in [3, 5, 7]:
+        #convertir l'index i en position x, y
         x, y = convertToGridIndex(i)
+        #Si grille x, y vaut 1:
         if grid[x][y]==1:
+            #incrémenter ligne de 1
             ligne=ligne+1
+        #Sinon si grille x, y vaut 2:
         elif grid[x][y]==2:
+            #décrementer ligne de 1
             ligne=ligne-1
+        #Sinon
         else:
+            #Si possibleLastBox ne vaut pas None:
             if possibleLastBox!=None:
+                #quitter la boucle
                 break
-            #if grid[x][y]!=2:
+            #mettre possibleLastBox à i
             possibleLastBox = i
+    #Si ligne vaut 2 et possibleLasteBox existe et que la valeur que renvoie getBoxValue(possibleLastBox) vaut 0:
     if ligne == 2 and possibleLastBox and getBoxValue(possibleLastBox)==0:
+        #mettre blockBox à possibleLastBox
         blockBox = possibleLastBox
+        #mettre ligne à 0
         ligne = 0
+        #mettre possibleLastBox à None
         possibleLastBox = None
+    #Sinon si ligne vaut -2 et possibleLasteBox existe et que la valeur que renvoie getBoxValue(possibleLastBox) vaut 0:
     elif ligne == -2 and possibleLastBox and getBoxValue(possibleLastBox)==0:
+        #mettre winBox à possibleLastBox
         winBox = possibleLastBox
+        #mettre ligne à 0
         ligne = 0
+        #mettre possibleLastBox à None
         possibleLastBox = None
+    #Sinon:
     else:
+        #mettre ligne à 0
         ligne = 0
+        #mettre possibleLastBox à None
         possibleLastBox = None
-
+    #créer une boucle de rangée 0 à 2 avec un index x
     for x in range(3):
+        #créer une boucle de rangée 0 à 2 avec un index y
         for y in range(3):
+            #Si la position x, y dans la grille vaut 1:
             if grid[x][y]==1:
+                #incrementer ligne de 1
                 ligne=ligne+1
+            #Sinon si la position x, y dans la grille vaut 2:
             elif grid[x][y]==2:
+                #décrementer ligne de 1
                 ligne=ligne-1
+            #Sinon:
             else:
+                #si possibleLastBox ne vaut pas None
                 if possibleLastBox!=None:
+                    #quitter la boucle
                     break
-                #if grid[x][y]!=2:
+                #assigner ce que retourne convertToNormalIndex à possibleLastBox
                 possibleLastBox = convertToNormalIndex(x, y)
+        #Si ligne vaut 2 et possibleLasteBox existe et que la valeur que renvoie getBoxValue(possibleLastBox) vaut 0:
         if ligne == 2 and possibleLastBox and getBoxValue(possibleLastBox)==0:
+            #mettre blockBox à possibleLastBox
             blockBox = possibleLastBox
+            #mettre ligne à 0
             ligne = 0
+            #mettre possibleLastBox à None
             possibleLastBox = None
+        #Sinon si ligne vaut -2 et possibleLasteBox existe et que la valeur que renvoie getBoxValue(possibleLastBox) vaut 0:
         elif ligne == -2 and possibleLastBox and getBoxValue(possibleLastBox)==0:
+            #mettre winBox à possibleLastBox
             winBox = possibleLastBox
+            #mettre ligne à 0
             ligne = 0
+            #mettre possibleLastBox à 0
             possibleLastBox = None
+        #Sinon:
         else:
+            #mettre ligne à 0
             ligne = 0
+            #mettre possibleLastBox à None
             possibleLastBox = None
 
     for y in range(3):
@@ -208,48 +271,87 @@ def ordiChoice():
             return i
     return random.randint(1, 9)
 
+#définir une fonction permettant de détecter si la grille est pleine
 def isGridFull():
+    #définir une variable boxTaken
     boxTaken = 0
+    #Pour x dans une rangée de  3
     for x in range(3):
+        #Pour y dans une rangée de 3
         for y in range(3):
+            #Si grille x, y ne vaut pas 0
             if grid[x][y]!=0:
+                #ajouter 1 a boxTaken
                 boxTaken += 1
+    #retourner boxTaken == 9 (true ou false)
     return boxTaken==9
-
+#afficher un message montrant que l'utilisateur possède les O et l'ordi les X
 print("User: O | CPU: X")
+#boucle infinie
 while True:
+    #Si playerTurn vaut true:
     if playerTurn:
+        #Tant qu'il n'y à pas de choix:
         while choice==None:
+            #afficher la grille
             printGrid()
+            #définir choix à une entrée utilisateur
             choice = input("It's your turn! (1-9) ")
+            #essayer de:
             try:
+                #convertir choice en entier
                 choice = int(choice)
+                #Si choice est inferieur à 1 ou supérieur à 9:
                 if choice<1 or choice>9:
+                    #mettre choix à None
                     choice = None
+                    #afficher un message disant qu'il faut un nombre entre 1 et 9
                     print("The user choice is out of range (must be between 1 and 9)")
+                #Si la case choisie est prise
                 if not checkGrid(choice):
+                    #mettre choix à None
                     choice = None
+                    #afficher un message disant que la case est occupée
                     print("The chosen case is already taken.")
+            #si l'essai echoue:
             except:
+                #mettre choix à None
                 choice = None
+                #afficher un message disant que la réponse doit être un entier
                 print("The user choice should be an integer")
+    #Sinon:
     else:
+        #afficher un message disant que l'IA doit jouer
         print("It's the ordi's turn!")
+        #Tant que choice vaut None ou que la case choisie est prise
         while choice==None or not checkGrid(choice):
+            #assigner ce que retourne ordiChoice à choice
             choice = ordiChoice()
+    #assigner à x, y ce que retourne convertToGridIndex(choice)
     x, y = convertToGridIndex(choice)
-
+    #assigner à la grid en position x, y 1 ou 2 suivant si playerTurn est True ou False
     grid[x][y] = playerTurn and 1 or 2
+    #mettre choice à None
     choice = None
+    #inverser playerTurn
     playerTurn = not playerTurn
-
+    #Si checkVictory(1) renvoie true:
     if checkVictory(1):
+        #afficher un message disant que l'utilisateur gagne
         print("\n\nThe user won!!")
+        #quitter la boucle
         break
+    #Sinon si checkVictory(2) renvoie true:
     elif checkVictory(2):
-        print("\n\nThe ordi won!")
+        #afficher un message disant que l'IA gagne
+        print("\n\nThe CPU won!")
+        #quitter la boucle
         break
+    #Sinon si isGridFull() renvoie true
     elif isGridFull():
+        #afficher une égalité
         print("Tie!")
+        #quitter la boucle
         break
+#afficher la grille
 printGrid()
